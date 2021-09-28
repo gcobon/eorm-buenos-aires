61b1475259be199b3355c41e516b81bd9056a8e0
Gudiel Cobon <gudielcobon@gmail.com>
2021-09-27 23:06:47 -0600

    home and admin module were updated

--- src/app/components/admin/admin-routing.module.ts
+++ src/app/components/admin/admin-routing.module.ts
@@ -2,10 +2,32 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminComponent } from './admin.component';
 
-const routes: Routes = [{ path: '', component: AdminComponent}];
+const routes: Routes = [
+  {
+    path: '',
+    component: AdminComponent,
+    children: [
+      {
+        path: 'users',
+        loadChildren: () =>
+          import('./user/user.module').then((m) => m.UserModule),
+      },
+      {
+        path: 'professors',
+        loadChildren: () =>
+          import('./profesor/profesor.module').then((m) => m.ProfesorModule),
+      },
+      {
+        path: 'students',
+        loadChildren: () =>
+          import('./student/student.module').then((m) => m.StudentModule),
+      },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AdminRoutingModule { }
+export class AdminRoutingModule {}
